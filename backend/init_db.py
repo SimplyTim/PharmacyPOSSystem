ab08b4d53644add7cccc105ecc84afa8dc813b15
Timothy Singh <timo3447@gmail.com>
2020-10-10 12:16:14 -0400

    reload backend

--- backend/init_db.py
+++
@@ -1,8 +0,0 @@
-from models import db, User, BloodCentre, Appointment
-from main import app
-
-db.drop_all()
-
-db.create_all(app=app)
-
-db.session.commit()
--- backend/models.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-import datetime
-
-
-db = SQLAlchemy(engine_options={"pool_recycle":60})
-
