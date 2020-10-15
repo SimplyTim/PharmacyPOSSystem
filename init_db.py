540e9fb297e5b61ed3ac90c96c9e3e523a785961
Timothy Singh <timo3447@gmail.com>
2020-10-14 23:04:12 -0400

    db setup

--- init_db.py
+++ init_db.py
@@ -1,8 +1,10 @@
-from models import db, User, BloodCentre, Appointment
+from models import db, Product, Employee, Transaction, Supplier
 from main import app
 
 db.drop_all()
-
 db.create_all(app=app)
+
+newUser1 = Employee(empFirstName='Timothy', empLastName='Singh', age=22, empType='General')
+newUser1.set_password("timpass")
 
 db.session.commit()
--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import json
-import jwt
 import datetime
 from flask_cors import CORS
 from flask import Flask, request, render_template, make_response, jsonify
 from sqlalchemy.exc import IntegrityError
 from datetime import timedelta 
 from functools import wraps
+from flask_jwt import JWT, jwt_required, current_identity
 from flask.views import MethodView
 
 
-from models import db
+from models import db, Product, Employee, Transaction, Supplier
 
-from models import db, User, BloodCentre, Appointment
 
 ''' Begin boilerplate code '''
 def create_app():
     app = Flask(__name__, static_url_path='')
-    CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.config['SECRET_KEY'] = "SECRET6555"
-
+    app.config['SECRET_KEY'] = "MYSECRET"
+    app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7)
+    CORS(app)
     db.init_app(app)
     return app
 
@@ -31,19 +30,17 @@ app.app_context().push()
 ''' End Boilerplate Code '''
 
 ''' Set up JWT here '''
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
+def authenticate(username, password):
+    user = Employee.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return user
 
-        if not token:
-            return jsonify({'message:' : 'Token was not found.'}), 403
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            return f(*args, **kwargs)
-        except:
-            return jsonify({'message' : 'Token is invalid'}), 403
-    return decorated
+#Payload is a dictionary which is passed to the function by Flask JWT
+def identity(payload):
+    return Employee.query.get(payload['identity'])
+
+jwt = JWT(app, authenticate, identity)
+''' End JWT Setup '''
 
 @app.route('/')
 def index():
