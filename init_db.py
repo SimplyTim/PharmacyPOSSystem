0c4cddaad7c2be7201a116c1e54287b7981b055b
Timothy Singh <timo3447@gmail.com>
2020-10-14 23:36:56 -0400

    database test

--- init_db.py
+++ init_db.py
@@ -6,6 +6,9 @@ db.create_all(app=app)
 
 newUser1 = Employee(empFirstName='Timothy', empLastName='Singh', age=22, empType='General')
 newUser1.set_password("timpass")
+newUser2 = Employee(empFirstName='Kumar', empLastName='Etwaroo', age=20, empType='General')
+newUser2.set_password("kumarpass")
 
 db.session.add(newUser1)
+db.session.add(newUser2)
 db.session.commit()
