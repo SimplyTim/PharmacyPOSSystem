f8d77dc16a039706ee6dac873ae0853ae9dc06b0
Timothy Singh <timo3447@gmail.com>
2020-10-14 23:15:10 -0400

    added heroku uri

--- backend/init_db.py
+++ backend/init_db.py
@@ -7,4 +7,5 @@ db.create_all(app=app)
 newUser1 = Employee(empFirstName='Timothy', empLastName='Singh', age=22, empType='General')
 newUser1.set_password("timpass")
 
+db.session.add(newUser1)
 db.session.commit()
