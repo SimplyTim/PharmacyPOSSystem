d719e3bf2704188c9be94d9e4d53834d965dbf28
Timothy Singh <timo3447@gmail.com>
2020-10-18 12:36:52 -0400

    heroku test 15

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def create_app():
     app = Flask(__name__, static_url_path='')
     app.config['SQLALCHEMY_DATABASE_URI'] = DBURI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.config['SECRET_KEY'] = SECRETKEY
+    app.config['JWT_SECRET_KEY'] = SECRETKEY
     app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7)
     CORS(app)
     db.init_app(app)
